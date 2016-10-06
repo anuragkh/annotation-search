74daa9b1a7c8909d72dec233aae7e5d618fb69da
Anurag Khandelwal <anuragk@berkeley.edu>
2016-10-06 00:40:04 -0700

    Removed addAnnotations, fixed error with metadata filter

--- src/main/scala/edu/berkeley/cs/succinct/annot/AnnotatedDocumentSerializer.scala
+++ src/main/scala/edu/berkeley/cs/succinct/annot/AnnotatedDocumentSerializer.scala
@@ -34,18 +34,47 @@ class AnnotatedDocumentSerializer(ignoreParseErrors: Boolean) extends Serializab
     delim + annotClass + delim + annotType + delim
   }
 
+  def decodeAnnotationString(annotStr: String): (String, (Int, Int, Int, String)) = {
+    val e = annotStr.split("\\^", 6)
+    val annotKey = makeKey(e(1), e(2))
+    val annotId = e(0).toInt
+    val startOffset = e(3).toInt
+    val endOffset = e(4).toInt
+    val metadata = if (e.length == 6) URLDecoder.decode(e(5), "UTF-8") else ""
+    (annotKey, (annotId, startOffset, endOffset, metadata))
+  }
+
+  def serializeAnnotationRecord(dat: Array[(Int, Int, Int, String)],
+                                baos: ByteArrayOutputStream): Unit = {
+    val out = new DataOutputStream(baos)
+
+    out.writeInt(dat.length)
+    dat.map(_._2).foreach(i => out.writeInt(i))
+    dat.map(_._3).foreach(i => out.writeInt(i))
+    dat.map(_._1).foreach(i => out.writeInt(i))
+    dat.map(_._4).foreach(i => {
+      if (i.length > Short.MaxValue && !ignoreParseErrors)
+        throw new InvalidPropertiesFormatException("Metadata too large: " + i.length)
+      val metadata = i.substring(0, Math.min(Short.MaxValue, i.length))
+      out.writeShort(metadata.length)
+      out.writeBytes(metadata)
+    })
+    out.flush()
+  }
+
+  def newAnnotData: (ArrayBuffer[Int], ArrayBuffer[Int], ByteArrayOutputStream) = {
+    (new ArrayBuffer[Int], new ArrayBuffer[Int], new ByteArrayOutputStream())
+  }
+
   def addAnnotations(docIdOffset: Int, docAnnotation: String): Unit = {
-    docAnnotation.split('\n').map(annot => annot.split("\\^", 6))
-      .map(e => (makeKey(e(1), e(2)), (e(0).toInt, e(3).toInt, e(4).toInt, if (e.length == 6) URLDecoder.decode(e(5), "UTF-8") else "")))
-      .groupBy(_._1)
-      .mapValues(v => AnnotatedDocumentSerializer.serializeAnnotationRecord(v.map(_._2).sortBy(_._2), ignoreParseErrors))
+    docAnnotation.split('\n').map(decodeAnnotationString).groupBy(_._1)
       .foreach(kv => {
         if (!docAnnotDataMap.contains(kv._1))
-          docAnnotDataMap += (kv._1 ->(new ArrayBuffer[Int], new ArrayBuffer[Int], new ByteArrayOutputStream()))
+          docAnnotDataMap += (kv._1 -> newAnnotData)
         val annotData = docAnnotDataMap(kv._1)
         annotData._1.append(docIdOffset)
         annotData._2.append(annotData._3.size())
-        annotData._3.write(kv._2)
+        serializeAnnotationRecord(kv._2.map(_._2).sortBy(_._2), annotData._3)
       })
   }
 
@@ -62,23 +91,4 @@ class AnnotatedDocumentSerializer(ignoreParseErrors: Boolean) extends Serializab
 
 object AnnotatedDocumentSerializer {
   val DELIM: Char = '^'
-
-  def serializeAnnotationRecord(dat: Array[(Int, Int, Int, String)], ignoreParseErrors: Boolean): Array[Byte] = {
-    val baos = new ByteArrayOutputStream
-    val out = new DataOutputStream(baos)
-
-    out.writeInt(dat.length)
-    dat.map(_._2).foreach(i => out.writeInt(i))
-    dat.map(_._3).foreach(i => out.writeInt(i))
-    dat.map(_._1).foreach(i => out.writeInt(i))
-    dat.map(_._4).foreach(i => {
-      if (i.length > Short.MaxValue && !ignoreParseErrors)
-        throw new InvalidPropertiesFormatException("Metadata too large: " + i.length + "; limit: " + Short.MaxValue)
-      val metadata = i.substring(0, Math.min(Short.MaxValue, i.length))
-      out.writeShort(metadata.length)
-      out.writeBytes(metadata)
-    })
-    out.flush()
-    baos.toByteArray
-  }
 }
