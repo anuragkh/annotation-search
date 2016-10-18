d82ec66694034fbd11c1537a608b0db625715b5c
Anurag Khandelwal <anuragk@berkeley.edu>
2016-10-17 22:00:53 -0700

    Added support for Spark 2.0

---
+++ src/main/scala/edu/berkeley/cs/succinct/annot/package.scala
@@ -0,0 +1,32 @@
+package edu.berkeley.cs.succinct
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+package object annot {
+
+  implicit class SuccinctContext(sc: SparkContext) {
+    def annotatedSuccinctFile(filePath: String, annotClassFilter: String, annotTypeFilter: String): AnnotatedSuccinctRDD = {
+      AnnotatedSuccinctRDD(sc, filePath, annotClassFilter, annotTypeFilter)
+    }
+  }
+
+  implicit class SuccinctSession(spark: SparkSession) {
+    def annotatedSuccinctFile(filePath: String, annotClassFilter: String, annotTypeFilter: String): AnnotatedSuccinctRDD = {
+      AnnotatedSuccinctRDD(spark.sparkContext, filePath, annotClassFilter, annotTypeFilter)
+    }
+  }
+
+  implicit class AnnotSuccinctRDD(rdd: RDD[(String, String, String)]) {
+    def succinctAnnotated: AnnotatedSuccinctRDD = {
+      AnnotatedSuccinctRDD(rdd)
+    }
+
+    def saveAsSuccinctFile(path: String, conf: Configuration = new Configuration()): Unit = {
+      AnnotatedSuccinctRDD(rdd).save(path, conf)
+    }
+  }
+
+}
