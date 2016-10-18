ba54a595c46add3df2f9763d9155b356abc8705c
Anurag Khandelwal <anuragk@berkeley.edu>
2016-10-17 23:23:50 -0700

    Persist RDD in construct script to save computation

--- src/main/scala/edu/berkeley/cs/succinct/annot/example/SparkConstruct.scala
+++ src/main/scala/edu/berkeley/cs/succinct/annot/example/SparkConstruct.scala
@@ -5,6 +5,7 @@ import edu.berkeley.cs.succinct.annot.AnnotatedSuccinctRDD
 import org.apache.hadoop.io._
 import org.apache.hadoop.mapred._
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.storage.StorageLevel
 
 object SparkConstruct {
   def main(args: Array[String]): Unit = {
@@ -12,7 +13,7 @@ object SparkConstruct {
     val outputPath = args(0) + ".succinct"
     val sc = new SparkContext(new SparkConf().setAppName("SparkConstruct"))
     val rdd = sc.hadoopFile[Text, StringArrayWritable, SequenceFileInputFormat[Text, StringArrayWritable]](inputPath)
-      .map(v => (v._1.toString, v._2.toStrings()(0), v._2.toStrings()(1)))
+      .map(v => (v._1.toString, v._2.toStrings()(0), v._2.toStrings()(1))).persist(StorageLevel.DISK_ONLY)
 
     println("Number of documents: " + rdd.count())
 
