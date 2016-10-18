1e7fe0c86fc8737688d80aec8ef99156a80cf88a
Anurag Khandelwal <anuragk@berkeley.edu>
2016-10-17 23:45:48 -0700

    Ask for number of partitions as an argument in the construct script

--- src/main/scala/edu/berkeley/cs/succinct/annot/example/SparkConstruct.scala
+++ src/main/scala/edu/berkeley/cs/succinct/annot/example/SparkConstruct.scala
@@ -4,16 +4,24 @@ import com.elsevier.cat.StringArrayWritable
 import edu.berkeley.cs.succinct.annot.AnnotatedSuccinctRDD
 import org.apache.hadoop.io._
 import org.apache.hadoop.mapred._
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkConstruct {
   def main(args: Array[String]): Unit = {
+
+    if (args.length != 2) {
+      println("Usage: construct [input-path] [num-partitions]")
+    }
+
     val inputPath = args(0)
     val outputPath = args(0) + ".succinct"
+    val numPartitions = args(1).toInt
     val sc = new SparkContext(new SparkConf().setAppName("SparkConstruct"))
     val rdd = sc.hadoopFile[Text, StringArrayWritable, SequenceFileInputFormat[Text, StringArrayWritable]](inputPath)
-      .map(v => (v._1.toString, v._2.toStrings()(0), v._2.toStrings()(1))).persist(StorageLevel.DISK_ONLY)
+      .map(v => (v._1.toString, v._2.toStrings()(0), v._2.toStrings()(1)))
+      .repartition(numPartitions)
+      .persist(StorageLevel.DISK_ONLY)
 
     println("Number of documents: " + rdd.count())
 
