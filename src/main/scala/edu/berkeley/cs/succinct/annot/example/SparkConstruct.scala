71ffe8601bfafa31dc15864ac9695d5a56e3a9de
Anurag Khandelwal <anuragk@berkeley.edu>
2016-10-22 18:33:26 -0700

    Updated construct script

--- src/main/scala/edu/berkeley/cs/succinct/annot/example/SparkConstruct.scala
+++ src/main/scala/edu/berkeley/cs/succinct/annot/example/SparkConstruct.scala
@@ -1,7 +1,7 @@
 package edu.berkeley.cs.succinct.annot.example
 
 import com.elsevier.cat.StringArrayWritable
-import edu.berkeley.cs.succinct.annot.AnnotatedSuccinctRDD
+import edu.berkeley.cs.succinct.annot._
 import org.apache.hadoop.io._
 import org.apache.hadoop.mapred._
 import org.apache.spark.{SparkConf, SparkContext}
@@ -19,11 +19,6 @@ object SparkConstruct {
     val rdd = sc.hadoopFile[Text, StringArrayWritable, SequenceFileInputFormat[Text, StringArrayWritable]](inputPath)
       .map(v => (v._1.toString, v._2.toStrings()(0), v._2.toStrings()(1)))
 
-    val succinctAnnot = AnnotatedSuccinctRDD(rdd)
-    println("Constructed Succinct data structures, saving to disk...")
-
-    succinctAnnot.save(outputPath)
-    println("Save to disk complete. Storage statistics: ")
-    succinctAnnot.printStorageStats()
+    rdd.saveAsAnnotatedSuccinctFile(outputPath)
   }
 }
