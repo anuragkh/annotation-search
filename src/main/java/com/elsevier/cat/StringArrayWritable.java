5180b2c05621d5e48101492b0b2522311f775f89
Anurag Khandelwal <anuragk@berkeley.edu>
2016-10-23 17:24:40 -0700

    Fixed formatting

--- src/main/java/com/elsevier/cat/StringArrayWritable.java
+++ src/main/java/com/elsevier/cat/StringArrayWritable.java
@@ -8,24 +8,25 @@ import org.apache.hadoop.io.Text;
 /**
  * Wrapper for ArrayWritable
  * This is needed so we can persist the array to S3.
- * 
- * @author mcbeathd
  *
+ * @author mcbeathd
  */
 public class StringArrayWritable extends ArrayWritable {
-	
-	// Logger
-	private static Log log = LogFactory.getLog(StringArrayWritable.class);
-	
-	public StringArrayWritable() { super(Text.class); }
-	
-	public StringArrayWritable(String[] strings) { 
-		super(Text.class);
-		Text[] texts = new Text[strings.length];
-	    for (int i = 0; i < strings.length; i++) {
-	        texts[i] = new Text(strings[i]);
-	    }
-	    set(texts);
-	}
+
+  // Logger
+  private static Log log = LogFactory.getLog(StringArrayWritable.class);
+
+  public StringArrayWritable() {
+    super(Text.class);
+  }
+
+  public StringArrayWritable(String[] strings) {
+    super(Text.class);
+    Text[] texts = new Text[strings.length];
+    for (int i = 0; i < strings.length; i++) {
+      texts[i] = new Text(strings[i]);
+    }
+    set(texts);
+  }
 
 }
